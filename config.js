3404d60affcf72d7f986efe48622738b5716c9bc
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2024-04-02 13:05:23 +0200

    le

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  s3: {
+    accessKeyId: 'SWLP4W2Z99TGXOH6DZKK', // TODO
+    secretAccessKey: 'wJKNRAtXr9qapTBaGvnYEOfAgzCIwUe906UBe8Pg', // TODO
+    endpoint: 's3.eu-west-2.wasabisys.com', // TODO
+    bucket: 'esgi-cloud-exam-bucket-36683', // TODO
+  },
+  suffix: {
+    small: '_small.jpg',
+    full: '_full.jpg'
+  }
+};
